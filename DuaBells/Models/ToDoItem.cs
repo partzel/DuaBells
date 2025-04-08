151e2852efac8ac9e9b5682d6b468aa87ca0ddcd
partzel <ahmedelmahdibendou2003@gmail.com>
2025-04-08 22:25:31 +0100

    feat: create ToDoItem model and view-model

---
+++ DuaBells/Models/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuaBells.Models
+{
+    /// <summary>
+    /// A basic data holder for the ToDo Items
+    /// </summary>
+    public class ToDoItem
+    {
+        public bool IsChecked { get; set; }
+        public string? Content { get; set; }
+    }
+}
---
+++ DuaBells/ViewModels/ToDoItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DuaBells.Models;
+
+namespace DuaBells.ViewModels
+{
+    public partial class ToDoItemViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private bool _isChecked;
+
+        [ObservableProperty]
+        private string? _content;
+
+
+        public ToDoItemViewModel() { }
+
+        public ToDoItemViewModel(ToDoItem item)
+        {
+            IsChecked = item.IsChecked;
+            Content = item.Content;
+        }
+    }
+}
