cd2c4ee46f4ceec4aa6f02fdcbe683a8a9729985
partzel <ahmedelmahdibendou2003@gmail.com>
2025-04-08 22:39:58 +0100

    feat: add ToDo item collection and utils

--- DuaBells/ViewModels/MainWindowViewModel.cs
+++ DuaBells/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,36 @@
-﻿namespace DuaBells.ViewModels
+﻿using DuaBells.Models;
+using DuaBells.ViewModels;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using CommunityToolkit.Mvvm.Input;
+
+namespace DuaBells.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        public ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = new();
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(AddItemCommand))]
+        private string? _newItemContent;
+
+
+        private bool CanAddItem() => (!string.IsNullOrEmpty(NewItemContent));
+
+        [RelayCommand(CanExecute = nameof(CanAddItem))]
+        private void AddItem()
+        {
+            var newItem = new ToDoItemViewModel
+            {
+                Content = NewItemContent
+            };
+
+            ToDoItems.Add(newItem);
+            NewItemContent = null;
+        }
+
+        [RelayCommand]
+        private void RemoveItem(ToDoItemViewModel item) => ToDoItems.Remove(item);
     }
 }
